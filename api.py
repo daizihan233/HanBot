28c54d48b6a4f270dbeafcac836941161a8529ad
HanTools <hantools@foxmail.com>
2022-03-06 18:35:26 +0800

    没话说了...

--- api.py
+++ api.py
@@ -6,64 +6,7 @@ from urllib import parse
 import aiohttp
 import asyncio
 import requests
-import json
-herbalist = [  # 祖安语录
-                '[CQ:image,file=file:///C:/FromHanTools/liaotian/img/jb.jpg]',
-                '你刚出生就被你父母抛弃不得不去乞讨结果乞讨到了一盆屎然后尼玛你爹被杀你又被人贩子带去解剖这就是你的傻逼一生',
-                '你就是歌姬吧',
-                '你妈死了',
-                '爪巴',
-                '傻逼一个',
-                '114514',
-                '1919810',
-                '1145141919810',
-                '1919810114514',
-                '1145141919810HOM',
-                '和你聊天真开心,送你一朵玫瑰花',
-                '先辈送福,新年快乐',
-                'nmd再@我一下试试！',
-                '滚',
-                '哎wcnmlgbd鬼！',
-                'fuck you',
-                '哎我cnmd谁tmd叫你m的让你个sb@我的？！！！',
-                'gun，傻逼',
-                '你礼貌吗？',
-                '脑残',
-                '操你妈',
-                '艹',
-                '我日你先人',
-                '我他娘的谢谢你啊',
-                '我********************',
-                '你是来吃屎的吧',
-                '一路走好，SB',
-                '你是啥玩意',
-                '你妈补天',
-                'TMD',
-                'CAO',
-                '大雨治水',
-                '你没妈',
-                '你没妈',
-                '马牛逼刚吃屎，你个马怂逼',
-                'SB',
-                '你死的好惨啊',
-                '孝出强大',
-                '宁真是个大孝子啊',
-                '你好骚啊',
-                '骚年，你爷爷在这',
-                '试试就逝世',
-                '全场目光向我看齐，我宣布一件事：你是傻逼',
-                '傻逼一号SB的你准备趋势',
-                '我操你妈',
-                '我不是人，但你绝对是狗',
-                '人家走到女生面前是夸好帅，而你走过去，会被骂变态',
-                '你好甩啊',  # 甩在南京话里指250
-                '去你妈',
-                '学校是我家，文明去你妈',
-                '学校是我家，文明靠大家',
-                '250',
-                '我屮艸芔茻你妈的',
-                'G他N的,一大逼抖子呼死你',
-            ]
+
 
 def isexists_dir_create(path):
     if not os.path.exists(path):
@@ -97,41 +40,43 @@ def send(msg, gid, uid=None):
         asyncio.run(no_at(msg, gid))
 
 
-def keyword(msg:str, uid, gid):
+def keyword(msg: str, uid, gid):
     if msg == '' or msg == ' ':
         send('嘿！这里是菜单\n'
-             '[1] 咕咕咕\n'
+             '[00] help\n'
+             '语法：@机器人 help [指令]\n'
+             '即可查看相关文档\n'
+             '[01] 咕咕咕\n'
              '请说：咕咕咕（当然可以是任何数量个咕）\n'
-             '[2] 黑名单\n'
+             '[02] 黑名单\n'
              '注意：此程序对空格尤为敏感\n'
              '注意：您必须有机器人管理员权限才能执行此功能\n'
              '语法1：@机器人【空格】黑名单【空格】@...（直接@）\n'
              '语法2：@机器人【空格】黑名单【空格】...（QQ号）\n'
-             '[3] 加群自动同意\n'
-             '（部分群可用，无需@，自动检测）\n'
-             '[4] 特定关键词复读\n'
+             '[03] 加群自动同意\n'
+             '（部分群可用）\n'
+             '[04] 特定关键词复读\n'
              '（无需@，一条消息必须只包含关键词）\n'
              '支持的关键词（“ | ”分割）：\n'
              'e | 额 | 呃 | 。 | w | www | 114514 | 1145141919810 | [CQ:face,id=298] '
              '| [CQ:face,id=277] | [CQ:face,id=178]\n'
-             '[5] 聊天（Beta试验版）\n'
+             '[05] 聊天\n'
              '（必须@，@尽量置前，不要加回复，尽量不要加表情，直接说内容）\n'
              '使用小i机器人API\n'
-             '[6] 祖安戳一戳 / 祖安我 / 祖安屑\n'
-             '当你戳一戳机器人或at机器人说“祖安我”、“祖安屑”的时候他会说一句祖安话\n'
-             '[7] 申请管理员\n'
+             '[06] 祖安戳一戳 / 祖安我 / 祖安@...\n'
+             '当你戳一戳机器人或at机器人说“祖安我”、“祖安屑”的时候机器人会对你/另一个人会说一句祖安话\n'
+             '[07] 申请管理员\n'
              '注：是机器人管理员，不是群管理员\n'
              '语法：@机器人 申请管理员\n'
-             '[8] 百度\n'
+             '[08] 百度\n'
              '让我帮你百度一下 :)\n'
              '百度对您来讲就这么难吗？？？\n'
              '语法：@机器人【空格】百度【空格】...（要搜的东西）\n'
              '把最后机器人发出来的网址发给你爱提问的朋友 :)\n'
-             '[9] 哔哩哔哩\n'
+             '[09] 哔哩哔哩\n'
              '让我帮你哔哩哔哩一下 :)\n'
-             'B站对您来讲就这么难吗？？？\n'
-             '语法：@机器人【空格】哔哩哔哩【空格】...（要搜的东西）\n'
-             '把最后机器人发出来的网址发给你爱提问的朋友 :)\n'
+             '[10] 改名监测\n'
+             '（部分群可用）'
              '========\n'
              'https://github.com/daizihan233/HanBot 这是这个机器人的代码，欢迎Star！\n'
              '========\n'
@@ -140,32 +85,36 @@ def keyword(msg:str, uid, gid):
              '注意：请仔细阅读文档，否则可能会出现意想不到的问题\n'
              '其它问题请联系作者QQ：183713750\n'
              '========\n'
-             '公告：https://shimo.im/docs/KqHXw8XrrwpXqGY9/',
-             gid, uid)
+             '公告：https://shimo.im/docs/KqHXw8XrrwpXqGY9/'
+             , gid, uid)
     else:
         if msg == '申请管理员':
-            requests.get('http://127.0.0.1:5700/send_private_msg?user_id=183713750&message='
-                         '【机器人管理员申请】\n'
-                         '[GID]: {}\n'
-                         '[UID]: {}'.format(gid, uid))
-            send('申请成功！ヾ(≧▽≦*)o请不要短时间内重复申请哦！开发能在后台看见哒~重复申请的话不会更快处理！！！（超大声）', gid, uid)
+            send('183713750 <<< 加他！\n'
+                 '👆 这个是机器人的开发\n'
+                 '👇 申请攻略：\n'
+                 '👉 详细地说明原因\n'
+                 '👉 保证不会恶意操作\n'
+                 '👉 保证保证不会滥用职权\n'
+                 '👉 已知如果滥用此权限会被撤销\n'
+                 '👉 已知在有前科的时候重新申请通过的概率会降低\n'
+                 '👉 已知申请成功的概率不是100%', gid, uid)
         elif ("群文件" == msg or "病毒库" == msg) and gid == 764869658:
             send(msg=
                  '''\nCN-xzf：https://xzfyyds.lanzoui.com/
-            OS相关:b02omemwh
- 浏览器(不经常更新):b02ok1xof
-           病毒库：b02ojc61a
-           OS激活相关：b02ojcf0d
-           驱动相关：b02ojckud
-           远程控制：b02ojcr4j
-           杀菌相关：b02ojnape
-           技术资料：b02ojnaxc
-           其他：b02ojj7kh
-    工具支持：蓝奏云   
-     PS：密码均为 666
+OS相关:b02omemwh
+浏览器(不经常更新):b02ok1xof
+病毒库：b02ojc61a
+OS激活相关：b02ojcf0d
+驱动相关：b02ojckud
+远程控制：b02ojcr4j
+杀菌相关：b02ojnape
+技术资料：b02ojnaxc
+其他：b02ojj7kh
+工具支持：蓝奏云 
+PS：密码均为 666
 群文件
-https://share.weiyun.com/VglthxSV
-    工具支持：腾讯微云''',
+https://share.weiyun.com/XvQofEc0
+工具支持：腾讯微云''',
                  gid=gid, uid=uid)
         elif msg[:3] == '百度 ':
             msg = msg.split(' ')
@@ -214,15 +163,13 @@ https://share.weiyun.com/VglthxSV
                                 requests.get('http://127.0.0.1:5700/send_group_msg?'
                                              'group_id={0}&'
                                              'message=[CQ:at,qq={1}] '
-                                             '{2}'.format(gid, uid, '{} 已在黑名单\n'
-                                                                    '（如果发现恶意添加请尽快联系HanTools删除）'.format(f)))
+                                             '{2}'.format(gid, uid, '{} 已在黑名单'.format(f)))
                             else:
                                 open('fucklist', 'a').write(f + '\n')
                                 requests.get('http://127.0.0.1:5700/send_group_msg?'
                                              'group_id={0}&'
                                              'message=[CQ:at,qq={1}] '
-                                             '{2}'.format(gid, uid, '已添加 {} 至黑名单\n'
-                                                                    '（如果发现恶意添加请尽快联系HanTools删除）'.format(f)))
+                                             '{2}'.format(gid, uid, '已添加 {} 至黑名单'.format(f)))
                     except:
                         send('error: 类型错误！QQ应该是int类型，但程序无法将其转为int', gid, uid)
             else:
@@ -248,8 +195,7 @@ https://share.weiyun.com/VglthxSV
                                 requests.get('http://127.0.0.1:5700/send_group_msg?'
                                              'group_id={0}&'
                                              'message=[CQ:at,qq={1}] '
-                                             '{2}'.format(gid, uid, '{} 已在黑名单\n'
-                                                                    '（如果发现恶意添加请尽快联系HanTools删除）'.format(f)))
+                                             '{2}'.format(gid, uid, '{} 已在黑名单'.format(f)))
                             else:
                                 requests.get('http://127.0.0.1:5700/send_private_msg?user_id=183713750&message='
                                              '【黑名单】\n'
@@ -274,6 +220,8 @@ https://share.weiyun.com/VglthxSV
                         tmp = int(tmp)
                         if tmp < 10000:
                             send('error: 参数错误！QQ号最小应该是10000', gid, uid)
+                        elif tmp == 183713750 or tmp == 748029973 or tmp == uid:
+                            send('error: 参数错误！无法添加此人', gid, uid)
                         else:
                             f = str(str(msg).split(' ')[-1])
                             fuck = open('fucklist', 'r').readlines()
